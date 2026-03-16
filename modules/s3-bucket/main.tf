1fb34f7a4e229c6505bc68b39b03da7382bf81b9
kalabtech <235978118+kalabtech@users.noreply.github.com>
2026-03-16 07:23:28 +0000

    feat(s3-module): add S3 bucket policy to enforce TLS-only transport

--- modules/s3-bucket/main.tf
+++ modules/s3-bucket/main.tf
@@ -43,3 +43,34 @@ resource "aws_s3_bucket_lifecycle_configuration" "this" {
     }
   }
 }
+
+# NOTE: Enforce TLS, HTTPS only
+data "aws_iam_policy_document" "this" {
+  statement {
+    sid    = "DenyInsecureTransport"
+    effect = "Deny"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = ["S3:*"]
+
+    resources = [
+      aws_s3_bucket.this.arn,
+      "${aws_s3_bucket.this.arn}/*",
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.this.json
+}
