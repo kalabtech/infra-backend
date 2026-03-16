57316564cd488d97fdb3aec86aa606cd35325a37
kalabtech <235978118+kalabtech@users.noreply.github.com>
2026-03-16 07:45:34 +0000

    feat(kms-module): add kms policy control root

--- modules/kms/main.tf
+++ modules/kms/main.tf
@@ -1,3 +1,6 @@
+data "aws_caller_identity" "this" {
+}
+
 resource "aws_kms_key" "this" {
   description             = "This key is used to encrypt bucket objects"
   deletion_window_in_days = 20
@@ -7,4 +10,22 @@ resource "aws_kms_key" "this" {
 resource "aws_kms_alias" "this" {
   name          = var.kms_alias_name
   target_key_id = aws_kms_key.this.key_id
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    sid    = "EnableRootAccess"
+    effect = "Allow"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.this.account_id}:root"]
+    }
+    actions   = ["kms:*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_kms_key_policy" "this" {
+  key_id = aws_kms_key.this.id
+  policy = data.aws_iam_policy_document.this.json
 }
